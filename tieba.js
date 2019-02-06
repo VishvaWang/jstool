cf463372e6ea70b390196971ab89b5ef474179d1
VishvaWang <vishva@qq.com>
2019-02-07 00:36:12 +0800

    百度贴吧清粉工具
    登录后在http://tieba.baidu.com/i/i/fans页面下打开控制台,输入代码执行.

---
+++ tieba.js
@@ -0,0 +1,16 @@
+(function (doneCount) {
+    for (user of Array.from(document.getElementsByClassName('user'))){
+        $.post("http://tieba.baidu.com/i/commit", {
+            cmd: "add_black_list",
+            portrait: user.attributes.portrait.value,
+            tbs: PageData.tbs,
+            ie: "utf-8"
+        },function(){
+            doneCount++;
+            if (doneCount==20){
+                $('#search_list').load(document.URL +  ' #search_list');
+                 arguments.callee.arguments.callee(0);
+            }
+        })
+    }
+})(0);
